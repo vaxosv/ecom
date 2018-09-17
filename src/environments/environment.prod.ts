9d81b1be643d83c2779e95ab41c2866c41fe4a30
vaxosv <vaxo.sv@gmaii.com>
2018-09-17 23:27:57 +0400

    prod build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAlcjdCQWVzYg7377vFj9Jf48clvEgcfAQ',
+    authDomain: 'fir-aa424.firebaseapp.com',
+    databaseURL: 'https://fir-aa424.firebaseio.com',
+    projectId: 'fir-aa424',
+    storageBucket: 'fir-aa424.appspot.com',
+    messagingSenderId: '9785689179'
+  }
 };
